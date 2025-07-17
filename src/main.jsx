b2b1c0533c35512768e656fce387ac483f8e43e9
もりしま <AKIHITO.MORISHIMA@mail.toyota-body.co.jp>
2025-07-17 23:50:39 +0900

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <img src="mo_square.png" />
+  </StrictMode>
+)
